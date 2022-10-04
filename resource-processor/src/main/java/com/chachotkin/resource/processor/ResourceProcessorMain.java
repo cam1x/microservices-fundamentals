e6143f886733083960f8ce4d5d77e1fbe2de18b8
Maksim Chachotkin <maxy.chechetkin@gmail.com>
2022-10-04 21:04:32 +0300

    Create skeleton for resource processor

---
+++ resource-processor/src/main/java/com/chachotkin/resource/processor/ResourceProcessorMain.java
@@ -0,0 +1,12 @@
+package com.chachotkin.resource.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceProcessorMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceProcessorMain.class, args);
+    }
+}
