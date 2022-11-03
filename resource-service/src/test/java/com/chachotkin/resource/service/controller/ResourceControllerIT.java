4592d71c24613945e88bc54dcd68442ca08329e4
Maksim Chachotkin <maxy.chechetkin@gmail.com>
2022-11-03 23:21:44 +0300

    Add integration tests

---
+++ resource-service/src/test/java/com/chachotkin/resource/service/controller/ResourceControllerIT.java
@@ -0,0 +1,98 @@
+package com.chachotkin.resource.service.controller;
+
+import com.chachotkin.resource.service.BaseIT;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.chachotkin.resource.service.util.AppConstants.AUDIO_CONTENT_TYPE;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ResourceControllerIT extends BaseIT {
+
+    private static final String RESOURCES_URL = "/resources";
+
+    private static final String MEDIA_FILE_PATH = "/media/file_example_2MB.mp3";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldUploadResource() throws Exception {
+        // given
+        var classPathResource = new ClassPathResource(MEDIA_FILE_PATH);
+        var multipartFile = new MockMultipartFile(
+                "file",
+                "file_example_2MB.mp3",
+                AUDIO_CONTENT_TYPE,
+                classPathResource.getInputStream().readAllBytes()
+        );
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.multipart(RESOURCES_URL).file(multipartFile))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(".id").isNotEmpty());
+    }
+
+    @Test
+    void shouldDownloadUploadedResource() throws Exception {
+        // given
+        var classPathResource = new ClassPathResource(MEDIA_FILE_PATH);
+        var fileContent = classPathResource.getInputStream().readAllBytes();
+        var multipartFile = new MockMultipartFile(
+                "file",
+                "file_example_2MB.mp3",
+                AUDIO_CONTENT_TYPE,
+                fileContent
+        );
+
+        var uploadResult = mockMvc.perform(MockMvcRequestBuilders.multipart(RESOURCES_URL).file(multipartFile))
+                .andReturn();
+        var uploadResponseContent = uploadResult.getResponse().getContentAsString();
+        var resourceId = JsonPath.read(uploadResponseContent, "$.id");
+
+        // when
+        mockMvc
+                .perform(MockMvcRequestBuilders.get(RESOURCES_URL + "/{id}", resourceId))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(AUDIO_CONTENT_TYPE))
+                .andReturn();
+    }
+
+    @Test
+    void shouldDeleteUploadedResource() throws Exception {
+        // given
+        var classPathResource = new ClassPathResource(MEDIA_FILE_PATH);
+        var fileContent = classPathResource.getInputStream().readAllBytes();
+        var multipartFile = new MockMultipartFile(
+                "file",
+                "file_example_2MB.mp3",
+                AUDIO_CONTENT_TYPE,
+                fileContent
+        );
+
+        var uploadResult = mockMvc.perform(MockMvcRequestBuilders.multipart(RESOURCES_URL).file(multipartFile))
+                .andReturn();
+        var uploadResponseContent = uploadResult.getResponse().getContentAsString();
+        var resourceId = JsonPath.read(uploadResponseContent, "$.id");
+
+        // when
+        mockMvc
+                .perform(MockMvcRequestBuilders.delete(RESOURCES_URL).param("ids", resourceId.toString()))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(".ids[0]").value(resourceId));
+    }
+}
